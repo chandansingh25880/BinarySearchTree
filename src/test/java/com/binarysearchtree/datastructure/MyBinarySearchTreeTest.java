f2e83a2fdcb9ce553548003287599e92c394d849
chandansingh25880 <chandan.1sep1997@gmail.com>
2021-04-20 23:44:51 +0530

    Added: To creat BST by adding 56 and then adding 30 & 70

--- src/test/java/com/binarysearchtree/datastructure/MyBinarySearchTreeTest.java
+++ src/test/java/com/binarysearchtree/datastructure/MyBinarySearchTreeTest.java
@@ -1,4 +1,19 @@
 package com.binarysearchtree.datastructure;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MyBinarySearchTreeTest {
+
+    /* @Description- To create a BST by adding 56 and then adding 30 and 70  */
+    @Test
+    public void given3NumbersWhenAddedToBinaryTreeShouldReturnSizeThree() {
+        BinarySearchTree<Integer> myBinaryTree = new BinarySearchTree();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        int size = myBinaryTree.getSize();
+        Assertions.assertEquals(3, size);
+
+    }
 }
