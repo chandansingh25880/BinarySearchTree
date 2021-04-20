cb59c41f9c3a7a9a0bf3fc483053645fa9167dd0
chandansingh25880 <chandan.1sep1997@gmail.com>
2021-04-20 23:15:30 +0530

    Added: Welcome to the binary search tree program

---
+++ src/main/java/com/binarysearchtree/datastructure/BinarySearchTree.java
@@ -0,0 +1,8 @@
+package com.binarysearchtree.datastructure;
+
+public class BinarySearchTree {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the binary search tree program");
+    }
+}
---
+++ src/test/java/com/binarysearchtree/datastructure/MyBinarySearchTreeTest.java
@@ -0,0 +1,4 @@
+package com.binarysearchtree.datastructure;
+
+public class MyBinarySearchTreeTest {
+}
