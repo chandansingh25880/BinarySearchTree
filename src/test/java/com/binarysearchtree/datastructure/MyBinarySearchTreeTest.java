0c064728bde6b300a1c57396c34edbc966072992
chandansingh25880 <chandan.1sep1997@gmail.com>
2021-04-21 00:20:40 +0530

    Added: Creating BST n no of Nodes given by Figure and getsize

--- src/test/java/com/binarysearchtree/datastructure/MyBinarySearchTreeTest.java
+++ src/test/java/com/binarysearchtree/datastructure/MyBinarySearchTreeTest.java
@@ -16,4 +16,26 @@ public class MyBinarySearchTreeTest {
         Assertions.assertEquals(3, size);
 
     }
+
+    /* @Description- To create a BST given by following condition */
+    @Test
+    public void givenNumbersWhenAddedToBinaryTreeShouldReturnSizeThirteen() {
+        BinarySearchTree<Integer> myBinaryTree = new BinarySearchTree<>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        myBinaryTree.add(22);
+        myBinaryTree.add(40);
+        myBinaryTree.add(60);
+        myBinaryTree.add(95);
+        myBinaryTree.add(11);
+        myBinaryTree.add(65);
+        myBinaryTree.add(3);
+        myBinaryTree.add(16);
+        myBinaryTree.add(63);
+        myBinaryTree.add(67);
+        int size = myBinaryTree.getSize();
+        Assertions.assertEquals(13, size);
+
+    }
 }
